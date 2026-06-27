931e86f0d0863f7520edb967489eb2ac088f1a56
tawnieraka-blip <tawnie.raka@gmail.com>
2026-06-27 21:05:33 +0300

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const calendarEl = document.getElementById("calendar");
+
+    const calendar = new FullCalendar.Calendar(calendarEl, {
+
+        locale: "ar",
+
+        direction: "rtl",
+
+        initialView: "dayGridMonth",
+
+        headerToolbar: {
+            left: "prev,next today",
+            center: "title",
+            right: ""
+        },
+
+        buttonText: {
+            today: "اليوم"
+        },
+
+        events: []
+
+    });
+
+    calendar.render();
+
+});
